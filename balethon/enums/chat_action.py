cdc0b19817e5f1a3ffd48be18583df04eb5826b5
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-10-11 16:47:20 +0330

    Add additional chat actions to `ChatAction` enum and enhance unwrap function for `NameEnum`

--- balethon/enums/chat_action.py
+++ balethon/enums/chat_action.py
@@ -5,3 +5,7 @@ from .name_enum import NameEnum
 
 class ChatAction(NameEnum):
     TYPING = auto()
+    UPLOAD_PHOTO = auto()
+    RECORD_VIDEO = auto()
+    UPLOAD_VIDEO = auto()
+    CHOOSE_STICKER = auto()
