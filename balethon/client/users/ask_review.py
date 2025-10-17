81d632b8e16094a01a68f70f9ff644b8930f6c07
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-10-17 22:02:29 +0330

    Update `ask_review` method to accept both integer and string user IDs for improved flexibility

--- balethon/client/users/ask_review.py
+++ balethon/client/users/ask_review.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import balethon
 
 
@@ -5,6 +7,7 @@ class AskReview:
 
     async def ask_review(
             self: "balethon.Client",
-            user_id: int
+            user_id: Union[int, str]
     ) -> bool:
+        user_id = await self.resolve_peer_id(user_id)
         return await self.auto_execute("post", "askReview", locals())
